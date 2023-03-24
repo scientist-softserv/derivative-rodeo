d9aceb0778f548b887086d112d5b74efa2f8a091
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-03-24 16:55:41 -0400

    Initial commit

---
+++ lib/space_stone/pdf_splitter.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "pdf_splitter/version"
+
+module SpaceStone
+  module PdfSplitter
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
