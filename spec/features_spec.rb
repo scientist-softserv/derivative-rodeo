dd23835832d135911618e1192d25fd6dac54c95d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2023-04-08 10:41:32 -0400

    Refactoring feature specs to prepare for more

--- spec/features_spec.rb
+++ spec/features_spec.rb
@@ -3,12 +3,23 @@
 require 'spec_helper'
 
 RSpec.describe "Features" do
-  let(:manifest) { Fixtures.pre_processing_manifest }
+  let(:manifest) do
+    Fixtures.pre_processing_manifest(
+    parent_identifier: parent_identifier,
+    original_filename: original_filename,
+    derivatives: derivatives,
+    mime_type: mime_type,
+    path_to_original: path_to_original
+  )
+  end
+
   let(:environment) { Fixtures.pre_processing_environment(manifest: manifest) }
   context "with a 2 page color PDF" do
-    let(:basename) { "sample-color-newsletter.pdf" }
+    let(:original_filename) { "sample-color-newsletter.pdf" }
     let(:parent_identifier) { "with-original-only" }
     let(:derivatives) { [:split_pdf] }
+    let(:mime_type) { "application/pdf" }
+    let(:path_to_original) { Fixtures.path_for(original_filename) }
 
     xit "splits the pages into images and extracts text" do
       environment.process_start!
@@ -16,7 +27,13 @@ RSpec.describe "Features" do
   end
 
   context "with a color image" do
-    let(:manifest) { Fixtures.pre_processing_manifest }
+    # Yes these are the defaults, but I'd rather be explicit in what we're doing.
+    let(:parent_identifier) { 'parent-identifier' }
+    let(:original_filename) { 'ocr_color.tiff' }
+    let(:derivatives) { { monochrome: Fixtures.path_for('ocr_gray.tiff') } }
+    let(:mime_type) { "image/tiff" }
+    let(:path_to_original) { Fixtures.path_for(original_filename) }
+
     it "runs the pre-processing and mime type processing" do
       environment.process_start!
 
