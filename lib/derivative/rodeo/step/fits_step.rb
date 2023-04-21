8375ee76b332d5b856949f6c4467b727f64faca7
Rob Kaufman <rob@notch8.com>
2023-04-21 15:58:54 -0700

    no fits, no problems

--- lib/derivative/rodeo/step/fits_step.rb
+++ lib/derivative/rodeo/step/fits_step.rb
@@ -1,24 +1,27 @@
 # frozen_string_literal: true
-require 'hydra/file_characterization'
 
-module Derivative
-  module Rodeo
-    module Step
-      class FitsStep < BaseStep
-        self.prerequisites = [:base_file_for_chain]
+begin
+  require 'hydra/file_characterization'
+rescue LoadError
+  puts "hydra/file_charaterization gem not found, no fits support"
+  module Derivative
+    module Rodeo
+      module Step
+        class FitsStep < BaseStep
+          self.prerequisites = [:base_file_for_chain]
 
-        # @see https://github.com/samvera/hyrax/blob/426575a9065a5dd3b30f458f5589a0a705ad7be2/app/models/concerns/hyrax/file_set/characterization.rb#L20-L24
-        def generate
-          # TODO: Leverage the path_for_shell_commands
-          file_system_path = arena.local_path_for_shell_commands(derivative: :base_file_for_chain)
-          content = File.read(file_system_path)
-          filename = File.basename(arena.file_set_filename)
+          # @see https://github.com/samvera/hyrax/blob/426575a9065a5dd3b30f458f5589a0a705ad7be2/app/models/concerns/hyrax/file_set/characterization.rb#L20-L24
+          def generate
+            # TODO: Leverage the path_for_shell_commands
+            file_system_path = arena.local_path_for_shell_commands(derivative: :base_file_for_chain)
+            content = File.read(file_system_path)
+            filename = File.basename(arena.file_set_filename)
 
-          arena.local_assign!(derivative: to_sym) do
-            Hydra::FileCharacterization.characterize(content, filename, to_sym)
+            arena.local_assign!(derivative: to_sym) do
+              Hydra::FileCharacterization.characterize(content, filename, to_sym)
+            end
           end
         end
       end
     end
   end
-end
